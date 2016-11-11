6fce345505cd79abcc287d883aa5fa45f2f43116
Charles De Barros <charlesdebarros@yahoo.co.uk>
2016-11-11 21:35:54 +0000

    Adding convert_num_to_string scripts.

---
+++ CodeWars_Python/convert_num_to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+
+def number_to_string(num):
+    return str(num)
+
+print(number_to_string(67))
---
+++ CodeWars_Ruby/convert_num_to_str.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+def num_to_string(num)
+  num.to_s
+end
+
+puts num_to_string(67)
